96cf05d74ade056c91044e86380ba44f7bc7ea6b
CosmicLegend77 <110524634+CosmicLegend77@users.noreply.github.com>
2022-08-06 22:55:35 +0530

    initial

--- app.js
+++ app.js
@@ -18,3 +18,49 @@ const swiper = new Swiper('.swiper', {
     },
   
 });
+
+var sidebarBtn = document.querySelector("#sidebar_btn")
+var sidebarClose = document.querySelector("#sidebar_close")
+var sidebar = document.querySelector("#sidebar")
+var overlay = document.querySelector("#overlay")
+
+sidebarBtn.addEventListener("click",function(){
+  sidebar.classList.add("active");
+  overlay.classList.add("active")
+})
+
+sidebarClose.addEventListener("click",function(){
+  sidebar.classList.remove("active");
+  overlay.classList.remove("active")
+})
+
+// accordian variables
+var accordianBtn = document.querySelectorAll("#accordian_menu")
+var accordian = document.querySelectorAll("#mobile_category_menu")
+
+for (let i = 0; i < accordianBtn.length; i++) {
+
+  accordianBtn[i].addEventListener('click', function () {
+
+    const clickedBtn = this.nextElementSibling.classList.contains('active');
+
+    for (let i = 0; i < accordian.length; i++) {
+
+      if (clickedBtn) break;
+
+      if (accordian[i].classList.contains('active')) {
+
+        accordian[i].classList.remove('active');
+        accordianBtn[i].classList.remove('active');
+
+      }
+
+    }
+
+    this.nextElementSibling.classList.toggle('active');
+    this.classList.toggle('active');
+
+  });
+
+}
+
